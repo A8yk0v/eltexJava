2cbdc4b10c1f3899d9a67dafbfa6600b5176ca9a
A8yk0v <AByk0v@yandex.ru>
2019-10-26 22:26:15 +0700

    Старт пятой лабы. Заготовки классов: IOrder, AManageOrder и Lab_5_main

---
+++ src/ru/eltex/app/java/lab_5/AManageOrder.java
@@ -0,0 +1,4 @@
+package ru.eltex.app.java.lab_5;
+
+public abstract class AManageOrder {
+}
---
+++ src/ru/eltex/app/java/lab_5/Lab_5_main.java
@@ -0,0 +1,8 @@
+package ru.eltex.app.java.lab_5;
+
+public class Lab_5_main {
+
+    public static void main(String[] args) {
+
+    }
+}
