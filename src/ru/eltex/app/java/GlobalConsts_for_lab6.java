edf7b46ccd4062086b4371ffcfabdac6b08407e3
A8yk0v <AByk0v@yandex.ru>
2019-11-09 17:06:46 +0700

    Класс с голобальными константами для 6-ой лабы

---
+++ src/ru/eltex/app/java/GlobalConsts_for_lab6.java
@@ -0,0 +1,10 @@
+package ru.eltex.app.java;
+
+public class GlobalConsts_for_lab6 {
+    public static final int SERVER_TCP_PORT = 4444;
+    public static final int UDP_PORT_FOR_INVITATION = 4445;
+    public static final int CLIENT_UDP_PORT_1 = 4446;
+    public static final String YOUR_ORDER_COMPLETE = "Your order complete";
+    public static final int SERVER_UDP_HELLO_TIMEOUT = 1000;
+    public static final int[] UDP_PORTS_FOR_INVITATION = {4446, 4447, 4448};
+}
